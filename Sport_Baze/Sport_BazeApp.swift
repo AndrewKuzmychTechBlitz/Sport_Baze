6ae37e3689ffe6707b514a94dba6705a0e7a0687
AndrewKuzmychTechBlitz <andrew.kuzmych@businessgroup.ltd>
2023-08-17 18:33:19 +0300

    Initial Commit

---
+++ Sport_Baze/Sport_BazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sport_BazeApp.swift
+//  Sport_Baze
+//
+//  Created by Андрій Кузьмич on 17.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Sport_BazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
